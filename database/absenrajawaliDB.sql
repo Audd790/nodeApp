43c592d02857314e14ead3fa3a39870748f1d1d2
Audd790 <10horysheet10@gmail.com>
2024-07-26 15:14:56 +0700

    add database

--- database/absenrajawaliDB.sql
+++ database/absenrajawaliDB.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `karyawan`
 --
-
+  
 DROP TABLE IF EXISTS `karyawan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
