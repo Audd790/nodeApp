483b68062b618da4f5786333ec5c01a932c4c2f7
Audd790 <10horysheet10@gmail.com>
2024-08-06 17:44:31 +0700

    can now format excel columns

--- routes/infograph.js
+++ routes/infograph.js
@@ -147,8 +147,36 @@ router.get('/formAbsen', (req,res)=>{
             connection.query(sql,(err,rows)=>{
                 const worksheet = XLSX.utils.json_to_sheet(rows);
                 const workbook = XLSX.utils.book_new();
-                XLSX.utils.book_append_sheet(workbook, worksheet, "Dates");
-                XLSX.writeFile(workbook, filePath, { compression: true });
+                var C = XLSX.utils.decode_col("G"); 
+                var fmt = 'dd/mm/yyyy hh:mm:ss'; // or '"$"#,##0.00_);[Red]\\("$"#,##0.00\\)' or any Excel number format
+
+                /* get worksheet range */
+                var range = XLSX.utils.decode_range(worksheet['!ref']);
+                for(var i = range.s.r + 1; i <= range.e.r; ++i) {
+                    /* find the data cell (range.s.r + 1 skips the header row of the worksheet) */
+                    var ref = XLSX.utils.encode_cell({r:i, c:C});
+                    /* if the particular row did not contain data for the column, the cell will not be generated */
+                    if(!worksheet[ref]) continue;
+                    /* assign the `.z` number format */
+                    worksheet[ref].z = fmt;
+                }
+
+                const COL_WIDTH = 50;
+
+                /* Excel column "C" -> SheetJS column index 2 == XLSX.utils.decode_col("C") */
+                var COL_INDEX = 2;
+                
+                /* create !cols array if it does not exist */
+                if(!worksheet["!cols"]) worksheet["!cols"] = [];
+                
+                /* create column metadata object if it does not exist */
+                if(!worksheet["!cols"][COL_INDEX]) worksheet["!cols"][COL_INDEX] = {wch: 8};
+                
+                /* set column width */
+                worksheet["!cols"][COL_INDEX].wpx = COL_WIDTH;
+                console.log(worksheet["!cols"][COL_INDEX].wpx)
+                XLSX.utils.book_append_sheet(workbook, worksheet, "Izin Karyawan");
+                XLSX.writeFile(workbook, filePath, { cellStyles: true});
             })
         } else{
             console.log("Exists")
